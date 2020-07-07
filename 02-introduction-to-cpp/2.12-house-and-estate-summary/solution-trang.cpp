afde849874fc33769514cb15d8d6de7bafb577b0
Jane Tran <tranjtrang@gmail.com>
2020-07-06 17:21:48 -0700

    Issue #1 - fix the negative sign before the $

--- 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
+++ 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
@@ -5,7 +5,10 @@ int main() {
    int currentPrice, lastMonthsPrice;
 
    cin >> currentPrice >> lastMonthsPrice;
-
+   if (currentPrice > lastMonthsPrice) {
+      cout << "This house is $" << currentPrice << ". The change is -$" << (lastMonthsPrice - currentPrice) * (-1) << " since last month." << endl;
+   }
+   else
    cout << "This house is $" << currentPrice << ". The change is -$" << lastMonthsPrice - currentPrice << " since last month." << endl;
 
    cout << "The estimated monthly mortgage is $" << (currentPrice * 0.051) / 12 << "." << endl;
