924226b3513d87dff9b308ce44a55738896e6ea3
Jane Tran <tranjtrang@gmail.com>
2020-07-07 21:23:09 -0700

    Fix 2.12

--- 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
+++ 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
@@ -6,10 +6,12 @@ int main() {
 
    cin >> currentPrice >> lastMonthsPrice;
    if (currentPrice > lastMonthsPrice) {
-      cout << "This house is $" << currentPrice << ". The change is -$" << (lastMonthsPrice - currentPrice) * (-1) << " since last month." << endl;
+      cout << "This house is $" << currentPrice << ". The change is $"
+           << (lastMonthsPrice - currentPrice) * (-1) << " since last month." << endl;
    }
    else
-   cout << "This house is $" << currentPrice << ". The change is -$" << lastMonthsPrice - currentPrice << " since last month." << endl;
+   cout << "This house is $" << currentPrice << ". The change is -$"
+        << lastMonthsPrice - currentPrice << " since last month." << endl;
 
    cout << "The estimated monthly mortgage is $" << (currentPrice * 0.051) / 12 << "." << endl;
 
