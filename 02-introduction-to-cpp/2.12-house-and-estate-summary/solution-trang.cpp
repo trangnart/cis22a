5ad3df17d8afe7b4dfd40a2a745f9514237e15b4
Paul Pham <paul@aquaron.com>
2020-07-07 23:27:07 -0700

    Issue #1 -- comment on coding style and practice.

--- 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
+++ 02-introduction-to-cpp/2.12-house-and-estate-summary/solution-trang.cpp
@@ -5,6 +5,35 @@ int main() {
    int currentPrice, lastMonthsPrice;
 
    cin >> currentPrice >> lastMonthsPrice;
+
+   /*** better formatting
+   if (currentPrice > lastMonthsPrice) {
+      cout << "This house is $"
+           << currentPrice
+           << ". The change is $"
+           << (lastMonthsPrice - currentPrice) * -1
+           << " since last month."
+           << endl;
+   } else {
+      cout << "This house is $"
+           << currentPrice
+           << ". The change is -$"
+           << lastMonthsPrice - currentPrice
+           << " since last month."
+           << endl;
+   }
+
+   cout << "The estimated monthly mortgage is $"
+        << (currentPrice * 0.051) / 12
+        << ".\n";
+   ***/
+
+   /*** BEST coding practice, see solution.cpp */
+
+   cout << "The estimated monthly mortgage is $"
+        << (currentPrice * 0.051) / 12
+        << ".\n";
+
    if (currentPrice > lastMonthsPrice) {
       cout << "This house is $" << currentPrice << ". The change is $"
            << (lastMonthsPrice - currentPrice) * (-1) << " since last month." << endl;
