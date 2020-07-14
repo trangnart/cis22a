805c25533486d366898bdd46497d3b705c8aec9d
Jane Tran <tranjtrang@gmail.com>
2020-07-13 20:32:39 -0700

    Program won't run if I do f(1), answer is correct

--- 03-expressions/3.28-optional-musical-note-frequencies/trang-solution.cpp
+++ 03-expressions/3.28-optional-musical-note-frequencies/trang-solution.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <iomanip>
-
+#include <cmath>
 using namespace std;
 
 int main() {
-
-   /* Type your code here. Include the math library above first. */
+   double f0;
+   double r = pow(2,(1.0/12.0));
+   double f_1 = f0 * pow(r,1);
+   double f_2 = f0 * pow(r,2);
+   double f_3 = f0 * pow(r,3);
+   double f_4 = f0 * pow(r,4);
+   
+   cin >> f0;
+   cout << fixed << setprecision(2);
+   cout << f_1 << " " << f_2 << " " << f_3 << " " << f_4 << endl;
 
    return 0;
 }
