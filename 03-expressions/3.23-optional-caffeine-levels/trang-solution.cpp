ec548a0e0daee9cb3889028d3466a17cf55925a1
Jane Tran <tranjtrang@gmail.com>
2020-07-21 14:50:55 -0700

    commit spacing changes 3.23

--- 03-expressions/3.23-optional-caffeine-levels/trang-solution.cpp
+++ 03-expressions/3.23-optional-caffeine-levels/trang-solution.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-   double caffeineMg;  // "double" supports floating-point like 75.5, versus int for integers like 75.
-   
-   cin >> caffeineMg;
+   double caffeineMg;
 
+   cin >> caffeineMg;
    cout << fixed << setprecision(2);
+   
    cout << "After 6 hours: " << caffeineMg / 2 << " mg" << endl;
    cout << "After 12 hours: " << caffeineMg / 4  << " mg" << endl;
    cout << "After 24 hours: " << caffeineMg / 16 << " mg" << endl;
-   
+
    return 0;
 }
