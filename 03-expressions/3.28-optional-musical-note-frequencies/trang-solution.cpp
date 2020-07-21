0862eb5519b6cd450425939b463230e747a682b7
Jane Tran <tranjtrang@gmail.com>
2020-07-21 12:23:17 -0700

    fix 3.28

--- 03-expressions/3.28-optional-musical-note-frequencies/trang-solution.cpp
+++ 03-expressions/3.28-optional-musical-note-frequencies/trang-solution.cpp
@@ -5,15 +5,16 @@ using namespace std;
 
 int main() {
    double f0;
+   cin >> f0;
+
    double r = pow(2,(1.0/12.0));
    double f_1 = f0 * pow(r,1);
    double f_2 = f0 * pow(r,2);
    double f_3 = f0 * pow(r,3);
    double f_4 = f0 * pow(r,4);
-   
-   cin >> f0;
-   cout << fixed << setprecision(2);
-   cout << f_1 << " " << f_2 << " " << f_3 << " " << f_4 << endl;
 
+   cout << fixed << setprecision(2);
+   cout << f0 << " " << f_1 << " " << f_2 << " " << f_3 << " " << f_4 << endl;
+   
    return 0;
 }
