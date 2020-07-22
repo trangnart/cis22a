76d5b8bd1fa3b29a6ff4e72dc0ccecda359d1af9
Paul Pham <paul@aquaron.com>
2020-07-21 23:22:27 -0700

    Issue #4 -- Solution for 4.21

--- 04-branches/4.21-optional-interstate-highway-numbers/solution.cpp
+++ 04-branches/4.21-optional-interstate-highway-numbers/solution.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     int highwayNumber;
+    string orientation = "north/south",
+           highwayType = "primary";
 
     cin >> highwayNumber;
+
+    // first, is this valid?
+    if (highwayNumber < 1 || highwayNumber > 999) {
+        cout << highwayNumber
+             << " is not a valid interstate highway number.\n";
+        return 1;
+    }
+    // now, all numbers are valid
+    if (highwayNumber % 2 == 0) {
+        // even: change orientation to east/west
+        orientation = "east/west";
+    }
+
+    if (highwayNumber >= 100) {
+        // 3 numbers: change type to aux
+        // % 100 to take only the remainder
+        highwayType = "auxiliary, Serving I-"
+                    + std::to_string(highwayNumber % 100);
+    }
+
+    // got all the parts, let put them together
+    cout << "I-" << highwayNumber
+         << " is " << highwayType
+         << ", going " << orientation << endl;
 
     return 0;
 }
