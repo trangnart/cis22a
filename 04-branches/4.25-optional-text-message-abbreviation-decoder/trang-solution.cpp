3e9a49e1269a6e41dc8c35217de8fff9440edfac
Jane Tran <tranjtrang@gmail.com>
2020-07-23 16:51:17 -0700

    issue #4 4.25

---
+++ 04-branches/4.25-optional-text-message-abbreviation-decoder/trang-solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string abbrev;
+    cin >> abbrev;
+    cout << "Input an abbreviation:" << endl << abbrev << endl;
+    
+    if (abbrev == "LOL")
+        cout << "laughing out loud" << endl;
+
+    else if (abbrev == "IDK")
+        cout << "I don't know" << endl;
+
+    else if (abbrev == "BFF")
+        cout << "best friends forever" << endl;
+
+    else if (abbrev == "IMHO")
+        cout << "in my humble opinion" << endl;
+
+    else if (abbrev == "TMI")
+        cout << "too much information" << endl;
+
+    else
+        cout << "Unknown" << endl;
+   return 0;
+}
