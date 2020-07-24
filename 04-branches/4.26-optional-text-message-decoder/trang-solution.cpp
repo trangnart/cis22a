c3f6904cc926c06dadda8844a27f771c07767a28
Jane Tran <tranjtrang@gmail.com>
2020-07-24 10:35:53 -0700

    issue #4 3.26 fix

--- 04-branches/4.26-optional-text-message-decoder/trang-solution.cpp
+++ 04-branches/4.26-optional-text-message-decoder/trang-solution.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 int main() {
     string user_str;
-    size_t IDK, BFF, JK, TMI, TTYL;
+    size_t abbrev;
 
     getline(cin, user_str);
     cout << "Enter text:\n" << user_str << endl
          << "You entered: " << user_str << endl;
 
-    IDK = user_str.find("IDK");
-    if (IDK != string::npos)
+    abbrev = user_str.find("IDK");
+    if (abbrev != string::npos)
         cout << "IDK: I don't know" << endl;
 
-    BFF = user_str.find("BFF");
-    if (BFF != string::npos)
+    abbrev = user_str.find("BFF");
+    if (abbrev != string::npos)
         cout << "BFF: best friend forever" << endl;
 
-    JK = user_str.find("JK");
-    if (JK != string::npos)
+    abbrev = user_str.find("JK");
+    if (abbrev != string::npos)
         cout << "JK: just kidding" << endl;
 
-    TMI = user_str.find("TMI");
-    if (TMI != string::npos)
+    abbrev = user_str.find("TMI");
+    if (abbrev != string::npos)
         cout << "TMI: too much information" << endl;
-        
-    TTYL = user_str.find("TTYL");
-    if (TTYL != string::npos)
+
+    abbrev = user_str.find("TTYL");
+    if (abbrev != string::npos)
         cout << "TTYL: talk to you later" << endl;
 
    return 0;
