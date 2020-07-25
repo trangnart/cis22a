312715d43f7d264ceb19fbf678d41f5dbdde289c
Jane Tran <tranjtrang@gmail.com>
2020-07-25 10:13:08 -0700

    issue #5 4.26 fix solution & style

--- 04-branches/4.26-optional-text-message-decoder/trang-solution.cpp
+++ 04-branches/4.26-optional-text-message-decoder/trang-solution.cpp
@@ -6,29 +6,29 @@ int main() {
     string user_str;
     size_t abbrev;
 
+    cout << "Enter text:" << endl;
     getline(cin, user_str);
-    cout << "Enter text:\n" << user_str << endl
-         << "You entered: " << user_str << endl;
+    cout << "You entered: " << user_str << endl;
 
     abbrev = user_str.find("IDK");
-    if (abbrev != string::npos)
+    if (abbrev != string::npos) {
         cout << "IDK: I don't know" << endl;
-
+    }
     abbrev = user_str.find("BFF");
-    if (abbrev != string::npos)
+    if (abbrev != string::npos) {
         cout << "BFF: best friend forever" << endl;
-
+    }
     abbrev = user_str.find("JK");
-    if (abbrev != string::npos)
+    if (abbrev != string::npos) {
         cout << "JK: just kidding" << endl;
-
+    }
     abbrev = user_str.find("TMI");
-    if (abbrev != string::npos)
+    if (abbrev != string::npos) {
         cout << "TMI: too much information" << endl;
-
+    }
     abbrev = user_str.find("TTYL");
-    if (abbrev != string::npos)
+    if (abbrev != string::npos) {
         cout << "TTYL: talk to you later" << endl;
-
+    }
    return 0;
 }
