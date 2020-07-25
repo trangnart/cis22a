78d92f10319901284df063b3cc09a529202da7ed
Jane Tran <tranjtrang@gmail.com>
2020-07-25 16:06:33 -0700

    issue #4 4.31

---
+++ 04-branches/4.31-complex-cost-structure/trang-solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int passengerAge;
+    int carryOns;
+    int checkedBags;
+    int airFare;
+
+    cin >> passengerAge;
+    cin >> carryOns;
+    cin >> checkedBags;
+
+    if (passengerAge < 3) {
+        airFare = 0;
+    }
+    else if (passengerAge > 59) {
+        airFare = 290;
+    }
+    else if (passengerAge < 60) {
+        airFare = 300;
+    }
+    if (carryOns == 1) {
+        airFare += 10;
+    }
+    if (checkedBags > 1) {
+        airFare += (checkedBags * 50)-25;
+    }
+
+    cout << airFare << endl;
+
+    return 0;
+}
