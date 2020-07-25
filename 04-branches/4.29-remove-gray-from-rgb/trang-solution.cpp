3785fa6a6846d6b4364a5783ece4bb5b75abfbc2
Jane Tran <tranjtrang@gmail.com>
2020-07-25 10:33:58 -0700

    issue #5 4.29 fix style

--- 04-branches/4.29-remove-gray-from-rgb/trang-solution.cpp
+++ 04-branches/4.29-remove-gray-from-rgb/trang-solution.cpp
@@ -5,17 +5,17 @@ int main() {
     int red, green, blue, smallest_val;
     cin >> red >> green >> blue;
 
-    if (red < green)
+    if (red < green) {
         smallest_val = red;
-
-    else if (green < blue)
+    }
+    else if (green < blue) {
         smallest_val = green;
-
-    else if (blue < red)
+    }
+    else if (blue < red) {
         smallest_val = blue;
-
-    cout << red - smallest_val << " " << green - smallest_val << " "
-         << blue - smallest_val << endl;
+    }
+    cout << red - smallest_val  << " " << green - smallest_val
+         << " " << blue - smallest_val << endl;
 
    return 0;
 }
