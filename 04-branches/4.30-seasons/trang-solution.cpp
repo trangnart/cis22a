d206387dcc61cc10ba2fdeea6e6be207b37a453b
Jane Tran <tranjtrang@gmail.com>
2020-07-25 16:06:11 -0700

    issue #4 4.30

--- 04-branches/4.30-seasons/trang-solution.cpp
+++ 04-branches/4.30-seasons/trang-solution.cpp
@@ -9,7 +9,37 @@ int main() {
    cin >> inputMonth;
    cin >> inputDay;
 
-   if (inputMonth)
+   if ((inputMonth == "March" && inputDay > 19 && inputDay < 32) ||
+      (inputMonth == "April" && inputDay > 0 && inputDay < 31) ||
+      (inputMonth == "May" && inputDay > 0 && inputDay < 32) ||
+      (inputMonth == "June" && inputDay > 0 && inputDay < 21)) {
+         cout << "Spring" << endl;
+   }
+
+   else if ((inputMonth == "June" && inputDay > 20 && inputDay < 31) ||
+      (inputMonth == "July" && inputDay > 0 && inputDay < 32) ||
+      (inputMonth == "August" && inputDay > 0 && inputDay < 32) ||
+      (inputMonth == "September" && inputDay > 0 && inputDay < 22)) {
+         cout << "Summer"<< endl;
+   }
+
+   else if ((inputMonth == "September" && inputDay > 21 && inputDay < 31) ||
+      (inputMonth == "October" && inputDay > 0 && inputDay < 32) ||
+      (inputMonth == "November" && inputDay > 0 && inputDay < 31) ||
+      (inputMonth == "December" && inputDay > 0 && inputDay < 21)) {
+         cout << "Autumn"<< endl;
+   }
+
+   else if ((inputMonth == "December" && inputDay > 20 && inputDay < 32) ||
+      (inputMonth == "January" && inputDay > 0 && inputDay < 32) ||
+      (inputMonth == "February" && inputDay > 0 && inputDay < 29) ||
+      (inputMonth == "March" && inputDay > 0 && inputDay < 20)) {
+         cout << "Winter"<< endl;
+   }
+   
+   else {
+      cout << "Invalid" << endl;
+   }
 
    return 0;
 }
