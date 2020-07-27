239642eb9355651845a14a963006359157c804b9
Jane Tran <tranjtrang@gmail.com>
2020-07-27 14:26:16 -0700

    issue #6 5.15

---
+++ 05-loops/5.15-optional-convert-to-binary/trang-solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int num, x;
+    string binary;
+
+    cin >> num;
+
+    while (num > 0) {
+        x = num % 2;
+        binary += to_string(x);
+        num = num / 2;
+    }
+    cout << binary;
+
+   return 0;
+}
