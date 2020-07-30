eb64c632b2ac31348c0a7952b8f8cd660789b78f
Paul Pham <paul@aquaron.com>
2020-07-29 22:34:34 -0700

    Issue #6 -- màu mè solution for 5.17

--- 05-loops/5.17-optional-brute-force-equation-solver/solution.cpp
+++ 05-loops/5.17-optional-brute-force-equation-solver/solution.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int x1, y1, sol1,
         x2, y2, sol2,
         x, y;
     cin >> x1 >> y1 >> sol1;
     cin >> x2 >> y2 >> sol2;
+    bool foundAnswer = false;
 
-    for (x = -10; x <= 10; ++x) {
-        for (y = -10; y <= 10; ++y) {
-            int test_sol1 = x1*x + y1*y;
-            int test_sol2 = x2*x + y2*y;
-            if (test_sol1 == sol1 && test_sol2 == sol2) {
+    for (x = -10; x <= 10 && !foundAnswer; ++x)
+    {
+        for (y = -10; y <= 10 && !foundAnswer; ++y)
+        {
+            int test_sol1 = x1 * x + y1 * y;
+            int test_sol2 = x2 * x + y2 * y;
+            if (test_sol1 == sol1 && test_sol2 == sol2)
+            {
                 cout << x << " " << y << endl;
-                return 0;
+                foundAnswer = true;
             }
         }
     }
-    cout << "No solution\n";
+    if (!foundAnswer)
+    {
+        cout << "No solution\n";
+    }
 
-    return 1;
+    return 0;
 }
