65351c030d6be6b79a472d87b16cfab037f53ece
Paul Pham <paul@aquaron.com>
2020-08-04 23:46:23 -0700

    Issue #8 -- solution for 6.09

--- 06-streams/6.09-optional-parsing-date/README.md
+++ 06-streams/6.09-optional-parsing-date/README.md
@@ -7,7 +7,13 @@ The input ends with `-1` on a line alone.
 Output each correct date as: `3/1/1990`.
 
 ## Example
+
 ### Input
+Input is the file called `dates.txt`.
+Include `fstream` and use `ifstream` to read the file instead of reading it
+from the keyboard.
+
+File content looks something like this:
 ```
 March 1, 1990
 April 2 1995
@@ -17,7 +23,16 @@ December 13, 2003
 ```
 
 ### Output
+Your program should run without needing nay user input from the keyboard and
+gives the output below:
 ```
 3/1/1990
 12/13/2003
 ```
+
+## Hint
+I'll let you use my `split()` function in Section [4.24] instead of
+doing your own space-finding.
+Figure out how to use it :)
+
+[4.24]: ../../04-branches/4.24-optional-name-format/paul-solution.cpp
--- 06-streams/6.09-optional-parsing-date/main.cpp
+++ 06-streams/6.09-optional-parsing-date/main.cpp
@@ -4,38 +4,38 @@
 using namespace std;
 
 int DateParser(string month) {
-	int monthInt = 0;
-	
-	if (month == "January")
-		monthInt = 1;
-	else if (month == "February")
-		monthInt = 2;
-	else if (month == "March")
-		monthInt = 3;
-	else if (month == "April")
-		monthInt = 4;
-	else if (month == "May")
-		monthInt = 5;
-	else if (month == "June")
-		monthInt = 6;
-	else if (month == "July")
-		monthInt = 7;
-	else if (month == "August")
-		monthInt = 8;
-	else if (month == "September")
-		monthInt = 9;
-	else if (month == "October")
-		monthInt = 10;
-	else if (month == "November")
-		monthInt = 11;
-	else if (month == "December")
-		monthInt = 12;
-	return monthInt;
+    int monthInt = 0;
+
+    if (month == "January")
+        monthInt = 1;
+    else if (month == "February")
+        monthInt = 2;
+    else if (month == "March")
+        monthInt = 3;
+    else if (month == "April")
+        monthInt = 4;
+    else if (month == "May")
+        monthInt = 5;
+    else if (month == "June")
+        monthInt = 6;
+    else if (month == "July")
+        monthInt = 7;
+    else if (month == "August")
+        monthInt = 8;
+    else if (month == "September")
+        monthInt = 9;
+    else if (month == "October")
+        monthInt = 10;
+    else if (month == "November")
+        monthInt = 11;
+    else if (month == "December")
+        monthInt = 12;
+    return monthInt;
 }
 
-int main () {
-	
-	// TODO: Read dates from input, parse the dates to find the one
-	//       in the correct format, and output in m/d/yyyy format
+int main() {
+
+    // TODO: Read dates from input, parse the dates to find the one
+    //       in the correct format, and output in m/d/yyyy format
 
 }
