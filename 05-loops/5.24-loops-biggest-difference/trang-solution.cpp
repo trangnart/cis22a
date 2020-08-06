d9e2441d60868c85871eb818ec12773a61374d16
Jane Tran <tranjtrang@gmail.com>
2020-08-06 10:37:58 -0700

    issue #7 5.24

---
+++ 05-loops/5.24-loops-biggest-difference/trang-solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int currNum, testDiff, sizeList;
+    int prevNum = 0;
+    int maxDiff = 0;
+
+    cin >> sizeList;
+
+    for (int i = 0; i < sizeList; ++i) {
+        cin >> currNum;
+        if (i > 0) {
+        testDiff = abs(currNum - prevNum);
+            if (testDiff > maxDiff) {
+                maxDiff = testDiff;
+            }
+        }
+        prevNum = currNum;
+    }
+
+    cout << maxDiff << endl;
+
+   return 0;
+}
