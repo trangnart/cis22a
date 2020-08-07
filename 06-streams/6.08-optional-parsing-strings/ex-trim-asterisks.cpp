fe7f78fbbe6090df96417bb2de07b520678d1dc5
Paul Pham <paul@aquaron.com>
2020-08-06 23:14:07 -0700

    Issue #8 -- Big help for 6.8

--- 06-streams/6.08-optional-parsing-strings/ex-trim-asterisks.cpp
+++ 06-streams/6.08-optional-parsing-strings/ex-trim-asterisks.cpp
@@ -2,11 +2,44 @@
 #include <string>
 using namespace std;
 
+string trim(string line);
+string get_right(string line);
+string get_left(string line);
+
 int main() {
     string line;
     cout << "Enter string (e.g. ***JANE**):\n";
     getline(cin, line);
 
+    size_t pos = line.find(',');
+    if (pos != string::npos) {
+        cout << "Hello "
+             << trim(get_left(line))
+             << " "
+             << trim(get_right(line))
+             << "!\n";
+    }
+
+    return 0;
+}
+
+string get_right(string line) {
+    size_t pos = line.find(',');
+    if (pos == string::npos) {
+        return "";
+    }
+    return line.substr(pos+1, line.length() - pos - 1);
+}
+
+string get_left(string line) {
+    size_t pos = line.find(',');
+    if (pos == string::npos) {
+        return "";
+    }
+    return line.substr(0, pos);
+}
+
+string trim(string line) {
     //         012345678
     // line = "***JANE**"
     // beg=3------^  ^-----end=6
@@ -15,17 +48,12 @@ int main() {
 
     // nothing that is not a * (bad)
     if (beg == string::npos) {
-        cout << "String \"" << line << "\" is invalid\n"
-             << "Enter a string surrounded by '*'\n";
-        return 1;
+        return "";
     }
 
     // it's ok if there's no '*' at the end
     size_t end = line.find_last_not_of('*');    // 6
     size_t len = end - beg + 1;
     string name = line.substr(beg, len);
-
-    cout << name << endl;
-
-    return 0;
+    return name;
 }
