cef5d06f923cb70a2f3b027f0cbf479d899fcd3d
Jane Tran <tranjtrang@gmail.com>
2020-08-24 13:59:18 -0700

    #10 7.24

---
+++ 07-functions/7.24-optional-swapping-variables/trang-solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void SwapValues(vector<int>& numbers);
+
+int main()
+{
+    vector<int> numbers(2);
+
+    for (auto& e : numbers) {
+        cin >> e;
+    }
+
+    SwapValues(numbers);
+
+    return 0;
+}
+
+void SwapValues(vector<int>& numbers) {
+    for (size_t i = 0; i < numbers.size()-1; ++i) {
+        int temp = numbers[0];
+        numbers[0] = numbers[1];
+        numbers[1] = temp;
+    }
+    for (auto& i : numbers) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
