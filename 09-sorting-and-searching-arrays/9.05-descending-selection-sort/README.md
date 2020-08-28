aabc2d8fe157af267d6b4e29e195492160e796ee
Paul Pham <paul@aquaron.com>
2020-08-28 13:27:49 -0700

    9.5 Definition and main (#12)

--- 09-sorting-and-searching-arrays/9.05-descending-selection-sort/README.md
+++ 09-sorting-and-searching-arrays/9.05-descending-selection-sort/README.md
@@ -1,7 +1,9 @@
 # 9.5 LAB: Descending selection sort with output during execution
 Write a void function `SelectionSortDescendTrace()`
 that takes an integer array, and sorts the array into descending order.
-The function should use nested loops and output the array after each iteration of the outer loop, thus outputting the array N-1 times (where N is the size). Complete main() to read in a list of up to 10 positive integers (ending in -1) and then call the SelectionSortDescendTrace() function.
+The function should use nested loops and output the array after each iteration of the outer loop, thus outputting the array `N`-1 times (where
+N is the size). Complete `main()` to read in a list of up to `10`
+positive integers (ending in `-1`) and then call the `SelectionSortDescendTrace()` function.
 
 If the input is:
 
--- 09-sorting-and-searching-arrays/9.05-descending-selection-sort/main.cpp
+++ 09-sorting-and-searching-arrays/9.05-descending-selection-sort/main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 // TODO: Write a void function SelectionSortDescendTrace() that takes
 //       an integer array and the number of elements in the array as arguments,
 //       and sorts the array into descending order.
-void SelectionSortDescendTrace(int numbers [], int numElements) {
-
+void SelectionSortDescendTrace(int numbers[], int numElements)
+{
 }
 
+int main(int argc, char *argv[])
+{
+    int input, i = 0;
+    int numElements = 0;
+    int numbers[10];
 
-int main(int argc, char* argv[]) {
-   int input, i = 0;
-   int numElements = 0;
-   int numbers [10];
+    // TODO: Read in a list of up to 10 positive integers; stop when
+    //       -1 is read. Then call SelectionSortDescendTrace() function.
 
-   // TODO: Read in a list of up to 10 positive integers; stop when
-   //       -1 is read. Then call SelectionSortDescendTrace() function.
-
-  return 0;
+    return 0;
 }
