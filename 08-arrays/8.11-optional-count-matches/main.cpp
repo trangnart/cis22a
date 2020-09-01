e4bc991b52afdd12946643d5e947059816d79ee3
Paul Pham <paul@aquaron.com>
2020-08-31 20:54:45 -0700

    8.11 new main.cpp (#11)

--- 08-arrays/8.11-optional-count-matches/main.cpp
+++ 08-arrays/8.11-optional-count-matches/main.cpp
@@ -2,31 +2,9 @@
 
 using namespace std;
 
-const int MAX_SIZE = 100;
-void ReadList(int list[], int &length);
-   /* Type your code here. */
+int CountMatches(int target, const vector<int>& vec);
 
-int main(int argc, char* argv[])
-{
-    int list[MAX_SIZE];
-    int length;    // actual size of list
-    int target, count;
-
-    cin >> target;
-    ReadList(list, length);
-    count = CountMatches(list, length, target);
-    cout << count << endl;
+int main() {
 
     return 0;
-}
-   /* Type your code here. */
-
-
-void ReadList(int list[], int &length)
-{
-    cin >> length;
-    for (int i = 0; i < length; i++)
-    {
-        cin >> list[i];
-    }
 }
