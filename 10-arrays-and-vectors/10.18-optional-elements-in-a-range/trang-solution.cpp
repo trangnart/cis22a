c1a4e40358e1781ef24ebc9afe8c667f727819ac
Jane Tran <tranjtrang@gmail.com>
2020-09-02 22:34:28 -0700

    10.18 solution #13

--- 10-arrays-and-vectors/10.18-optional-elements-in-a-range/trang-solution.cpp
+++ 10-arrays-and-vectors/10.18-optional-elements-in-a-range/trang-solution.cpp
@@ -2,9 +2,32 @@
 #include <vector>
 using namespace std;
 
-int main() {
+void PopulateVector(vector<int>& numbers);
 
-   /* Type your code here. */
+int main() {
+   vector<int> numbers;
+   string range;
+   int min, max;
+
+   PopulateVector(numbers);
+   cin >> min >> max;
+
+   for (auto& e : numbers){
+      if (e >= min && e <= max) {
+         cout << e << " ";
+      }
+   }
+   cout << endl;
 
    return 0;
+}
+
+void PopulateVector(vector<int>& numbers) {
+   int size;
+   cin >> size;
+
+   numbers.resize(size);
+   for (auto& e : numbers) {
+      cin >> e;
+   }
 }
