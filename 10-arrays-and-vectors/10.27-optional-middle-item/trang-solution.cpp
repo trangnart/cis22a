684a500031cce0c7fbd4f8d6f3e7b7d90bbe3f28
Jane Tran <tranjtrang@gmail.com>
2020-09-04 11:59:01 -0700

    10.27 solution #14

---
+++ 10-arrays-and-vectors/10.27-optional-middle-item/trang-solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+void PopulateVector(vector<int> &numbers);
+
+int main()
+{
+    vector<int> numbers;
+
+    PopulateVector(numbers);
+
+    if (numbers.size() < 10) {
+        int middle = numbers.size() / 2;
+        cout << numbers[middle] << endl;
+    }
+    else {
+        cout << "Too many inputs" << endl;
+    }
+
+    return 0;
+}
+
+void PopulateVector(vector<int> &numbers)
+{
+    int num;
+    cin >> num;
+
+    while (num != -1)
+    {
+        numbers.push_back(num);
+        cin >> num;
+    }
+}
