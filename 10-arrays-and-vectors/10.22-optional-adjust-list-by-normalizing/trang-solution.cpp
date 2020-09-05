cacfe075f21c780983542be40a0168997e43e97d
Jane Tran <tranjtrang@gmail.com>
2020-09-04 22:33:14 -0700

    10.22 add function #13

--- 10-arrays-and-vectors/10.22-optional-adjust-list-by-normalizing/trang-solution.cpp
+++ 10-arrays-and-vectors/10.22-optional-adjust-list-by-normalizing/trang-solution.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
+#include <sstream>
+#include <string>
+#include <iterator>
 #include <algorithm>
 using namespace std;
 
@@ -14,9 +16,11 @@ int main()
     vector<int> numbers;
 
     PopulateVector(numbers);
+
     int min = GetMinimumInt(numbers);
     SubtractMin(min, numbers);
 
+    cout << Vector2String(numbers) << endl;
     return 0;
 }
 
@@ -32,7 +36,8 @@ void PopulateVector(vector<int> &numbers)
     }
 }
 
-int GetMinimumInt(vector<int> listInts) {
+int GetMinimumInt(vector<int> listInts)
+{
     auto min = min_element(listInts.begin(), listInts.end());
     return *min;
 }
@@ -41,12 +46,15 @@ void SubtractMin(int min, vector<int> &listInts)
 {
     for (size_t i = 0; i < listInts.size(); ++i)
     {
-        listInts[i] - min;
+        listInts[i] = listInts[i] - min;
     }
 }
 
-string Vector2String(const vector<int> &listInts, const char * = " ") {
+string Vector2String(const vector<int> &listInts, const char *delim)
+{
     stringstream oss;
+
     copy(listInts.begin(), listInts.end(),
-         ostream_iterator<int>)
+         ostream_iterator<int>(oss, delim));
+    return oss.str();
 }
