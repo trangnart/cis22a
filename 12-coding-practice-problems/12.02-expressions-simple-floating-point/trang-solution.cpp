ac6bc33fb8a1a240c8fd09ce4b628bb95fc09578
Jane Tran <tranjtrang@gmail.com>
2020-09-07 16:46:30 -0700

    12.02 solution #15

---
+++ 12-coding-practice-problems/12.02-expressions-simple-floating-point/trang-solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+   double y;
+
+   cin >> x;
+
+   y = (1.0 / 3.0) * x + (x / 4.0) + 2.0 * x;
+
+   cout << y << endl;
+
+   return 0;
+}
