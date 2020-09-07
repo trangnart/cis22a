5c1a1550e40e080d29f7fda7e3cb62245c422aeb
Jane Tran <tranjtrang@gmail.com>
2020-09-07 16:42:57 -0700

    12.01 solution #15

---
+++ 12-coding-practice-problems/12.01-practice/trang-solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userInt;
+    cin >> userInt;
+
+    for (int i = 0; i < 2; ++i) {
+        cout << userInt << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
