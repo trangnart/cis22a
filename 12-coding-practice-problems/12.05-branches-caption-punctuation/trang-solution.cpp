e6be09b036fb7f11867ffd77113f85c61a39ed37
Jane Tran <tranjtrang@gmail.com>
2020-09-07 21:27:31 -0700

    12.05 remove commas #15

--- 12-coding-practice-problems/12.05-branches-caption-punctuation/trang-solution.cpp
+++ 12-coding-practice-problems/12.05-branches-caption-punctuation/trang-solution.cpp
@@ -8,11 +8,15 @@ int main()
     string userCaption;
     getline(cin, userCaption);
     size_t len = userCaption.size();
-
+    char commas = userCaption.back();
     char first_punct = userCaption[len - 3];
     char second_punct = userCaption[len - 2];
     char last_punct = userCaption[len - 1];
 
+    if (commas == ',')
+    {
+        userCaption.pop_back();
+    }
     if (last_punct != '.' && last_punct != '!' && last_punct != '?')
     {
         userCaption.push_back('.');
