8574fdb256ba52575dc051479f51dd2aeac563af
Jane Tran <tranjtrang@gmail.com>
2020-09-07 21:14:37 -0700

    10.05 solutiion #14

---
+++ 12-coding-practice-problems/12.05-branches-caption-punctuation/trang-solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string userCaption;
+    getline(cin, userCaption);
+    size_t len = userCaption.size();
+
+    char first_punct = userCaption[len - 3];
+    char second_punct = userCaption[len - 2];
+    char last_punct = userCaption[len - 1];
+
+    if (last_punct != '.' && last_punct != '!' && last_punct != '?')
+    {
+        userCaption.push_back('.');
+    }
+    if (last_punct == '.' && second_punct == '.' && first_punct != '.')
+    {
+        userCaption.pop_back();
+    }
+
+    cout << userCaption << endl;
+
+    return 0;
+}
