5b5676f7ba2c6d93f2849bf01d0a39264e911825
Jane Tran <tranjtrang@gmail.com>
2020-09-09 00:01:22 -0700

    12.09 add valid input range #15

--- 12-coding-practice-problems/12.09-branches-runway-headings/trang-solution.cpp
+++ 12-coding-practice-problems/12.09-branches-runway-headings/trang-solution.cpp
@@ -6,37 +6,44 @@ int main()
     string direction = "northeast";
     int x;
     cin >> x;
-    int runwayDeg = x * 10;
-
-    if ((runwayDeg > -23) && (runwayDeg < 23))
-    {
-        direction = "north";
-    }
-    else if ((runwayDeg > 67) && (runwayDeg < 113))
-    {
-        direction = "east";
-    }
-    else if ((runwayDeg > 112) && (runwayDeg < 158))
-    {
-        direction = "southeast";
-    }
-    else if ((runwayDeg > 157) && (runwayDeg < 203))
-    {
-        direction = "south";
-    }
-    else if ((runwayDeg > 202) && (runwayDeg < 248))
-    {
-        direction = "southwest";
-    }
-    else if ((runwayDeg > 247) && (runwayDeg < 293))
-    {
-        direction = "west";
-    }
-    else if ((runwayDeg > 292) && (runwayDeg < 338))
-    {
-        direction = "northwest";
-    }
-    cout << runwayDeg << " degrees (" << direction << ")" << endl;
     
+    if (x > 0 && x < 37) {
+        int runwayDeg = x * 10;
+
+        if ((runwayDeg > -23) && (runwayDeg < 23))
+        {
+            direction = "north";
+        }
+        else if ((runwayDeg > 67) && (runwayDeg < 113))
+        {
+            direction = "east";
+        }
+        else if ((runwayDeg > 112) && (runwayDeg < 158))
+        {
+            direction = "southeast";
+        }
+        else if ((runwayDeg > 157) && (runwayDeg < 203))
+        {
+            direction = "south";
+        }
+        else if ((runwayDeg > 202) && (runwayDeg < 248))
+        {
+            direction = "southwest";
+        }
+        else if ((runwayDeg > 247) && (runwayDeg < 293))
+        {
+            direction = "west";
+        }
+        else if ((runwayDeg > 292) && (runwayDeg < 338))
+        {
+            direction = "northwest";
+        }
+        cout << runwayDeg << " degrees (" << direction << ")" << endl;
+    }
+
+    else {
+        cout << "The valid inputs are 1-36." << endl;
+    }
+
     return 0;
 }
