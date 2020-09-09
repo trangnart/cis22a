04a4d2b86e5fd40959ed09de4e402ced81db22c5
Jane Tran <tranjtrang@gmail.com>
2020-09-08 23:54:41 -0700

    12.09 solution #15

---
+++ 12-coding-practice-problems/12.09-branches-runway-headings/trang-solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string direction = "northeast";
+    int x;
+    cin >> x;
+    int runwayDeg = x * 10;
+
+    if ((runwayDeg > -23) && (runwayDeg < 23))
+    {
+        direction = "north";
+    }
+    else if ((runwayDeg > 67) && (runwayDeg < 113))
+    {
+        direction = "east";
+    }
+    else if ((runwayDeg > 112) && (runwayDeg < 158))
+    {
+        direction = "southeast";
+    }
+    else if ((runwayDeg > 157) && (runwayDeg < 203))
+    {
+        direction = "south";
+    }
+    else if ((runwayDeg > 202) && (runwayDeg < 248))
+    {
+        direction = "southwest";
+    }
+    else if ((runwayDeg > 247) && (runwayDeg < 293))
+    {
+        direction = "west";
+    }
+    else if ((runwayDeg > 292) && (runwayDeg < 338))
+    {
+        direction = "northwest";
+    }
+    cout << runwayDeg << " degrees (" << direction << ")" << endl;
+    
+    return 0;
+}
