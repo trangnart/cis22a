bbd981a3ddca5f990491de3790bdc347e3da80f7
Jane Tran <tranjtrang@gmail.com>
2020-12-16 13:24:01 -0800

    remove if 12.18 #17

--- 12-coding-practice-problems/12.18-loops-removing-leading-trailing-whitespace/trang_solution.cpp
+++ 12-coding-practice-problems/12.18-loops-removing-leading-trailing-whitespace/trang_solution.cpp
@@ -9,9 +9,8 @@ int main() {
     size_t fn_space = text.find_first_not_of(" ");
     size_t ln_space = text.find_last_not_of(" ");
 
-    if(fn_space != string::npos || ln_space != string::npos) {
-        text = text.substr(fn_space, ln_space + 1);
-    }
+    text = text.substr(fn_space, ln_space + 1);
+
     cout << text << endl;
 
     return 0;
