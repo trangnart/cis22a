6e89598727ea76711df06f27181cad0bc573785d
Jane Tran <tranjtrang@gmail.com>
2020-12-16 14:49:19 -0800

    12.22 trang solution #18

---
+++ 12-coding-practice-problems/12.22-loops-customer-lookup/trang_solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string idToFind;
+    string dbId;
+    string dbFirstName;
+    string dbLastName;
+    ifstream customerDb;
+
+    cin >> idToFind;
+
+    customerDb.open("CustomerDb.txt");
+    if (customerDb.fail()) {
+        cout << "Could not open CustomerDb.txt" << endl;
+    }
+    else {
+        while ((customerDb >> dbId) && (dbId != idToFind)) {
+            continue;
+        }
+
+        if (idToFind == dbId) {
+            customerDb >> dbFirstName >> dbLastName;
+            cout << dbFirstName << " " << dbLastName << endl;
+        }
+        else {
+            cout << "Not found" << endl;
+        }
+    customerDb.close();
+    }
+
+    return 0;
+}
