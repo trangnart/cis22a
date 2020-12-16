8fd2a059fd26e37355d1b4c9eeb817999d21ab92
Jane Tran <tranjtrang@gmail.com>
2020-12-16 13:22:14 -0800

    #17 fix 12.18

--- 12-coding-practice-problems/12.18-loops-removing-leading-trailing-whitespace/trang_solution.cpp
+++ 12-coding-practice-problems/12.18-loops-removing-leading-trailing-whitespace/trang_solution.cpp
@@ -6,11 +6,13 @@ int main() {
     string text;
     getline(cin, text);
 
-    int fn_space = text.find_first_not_of(" ");
-    int ln_space = text.find_last_not_of(" ");
+    size_t fn_space = text.find_first_not_of(" ");
+    size_t ln_space = text.find_last_not_of(" ");
 
-    text = text.substr(fn_space, ln_space);
-    cout << "/" << text << "/" << endl;
+    if(fn_space != string::npos || ln_space != string::npos) {
+        text = text.substr(fn_space, ln_space + 1);
+    }
+    cout << text << endl;
 
     return 0;
 }
