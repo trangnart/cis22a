24aded8de84326acd4d1e71c9d93ae606008db00
Jane Tran <tranjtrang@gmail.com>
2020-12-17 13:45:29 -0800

    12.24 fix for loop #18

--- 12-coding-practice-problems/12.24-loops-academic-index-score-table/trang_solution.cpp
+++ 12-coding-practice-problems/12.24-loops-academic-index-score-table/trang_solution.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main() {
-    double AIS, GPA = 2.9, threshold, test_score;
+    double AIS, threshold, test_score;
     cin >> threshold;
     cout << fixed << setprecision(1);
 
-    for (int i = 0; i < 11; i++) {
-        GPA += 0.1;
+    for (double GPA = 3.0; GPA < 4.1; GPA+=0.1) {
         test_score = ((static_cast<double>(threshold) - (2.5 * (GPA / 4.0) * 100.0)) / 100.0) * 1600.0;
         cout << GPA << " " << test_score << endl;
     }
