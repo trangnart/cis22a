a41c11a35b9c2f8ded06c86e99d5e9e1eb1694d1
Jane Tran <tranjtrang@gmail.com>
2020-12-17 11:35:09 -0800

    #18 trang solution 12.24

---
+++ 12-coding-practice-problems/12.24-loops-academic-index-score-table/trang_solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double AIS, GPA = 2.9, threshold, test_score;
+    cin >> threshold;
+    cout << fixed << setprecision(1);
+
+    for (int i = 0; i < 11; i++) {
+        GPA += 0.1;
+        cout << GPA << " ";
+        test_score = GPA / threshold;
+    }
+    AIS = 2.5 * (GPA / 4.0) * 100 + (test_score / 1600) * 100;
+    cout << endl << AIS << endl;
+    return 0;
+}
