ec4075084693ade4073c73d185d0dacda8416d25
Jane Tran <tranjtrang@gmail.com>
2020-12-16 22:13:08 -0800

    use stringstream 12.23 #18

--- 12-coding-practice-problems/12.23-loops-word-count/trang_solution.cpp
+++ 12-coding-practice-problems/12.23-loops-word-count/trang_solution.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main() {
+    int count = 0;
     string text;
-    int count = 1;
     getline(cin, text);
+    stringstream ss(text);
 
-    for (size_t i = 0; i < text.length(); i++) {
-        if(text[i] == ' ') {
-            count++;
-        }
+    while(ss >> text) {
+        count++;
     }
-    cout << count << endl;
+        cout << count << endl;
 
     return 0;
 }
