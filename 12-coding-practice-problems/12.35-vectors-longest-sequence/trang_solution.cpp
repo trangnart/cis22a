9e7d1e9d14b1adf8ca2d9108fc2868de176aff22
Jane Tran <tranjtrang@gmail.com>
2020-12-22 22:08:08 -0800

    12.35 trang solution #19

---
+++ 12-coding-practice-problems/12.35-vectors-longest-sequence/trang_solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int numItems;
+    vector<string> listItems;
+    string currItem;
+    int i, count = 0, maxLength = 0;
+
+    cin >> numItems;
+    for (i = 0; i < numItems; ++i) {
+        cin >> currItem;
+        listItems.push_back(currItem);
+    }
+
+    for (i = 0; i < numItems; i++) {
+        currItem = listItems.at(i);
+        if (currItem != "I") {
+            count++;
+        }
+        else if (count > maxLength) {
+            maxLength = count;
+            count = 0;
+        }
+    }
+    cout << maxLength << endl;
+    
+    return 0;
+}
