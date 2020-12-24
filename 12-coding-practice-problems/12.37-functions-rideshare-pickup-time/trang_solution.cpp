0caef46229928257ab4aed6381b8ec75b5a8c676
Jane Tran <tranjtrang@gmail.com>
2020-12-23 22:29:38 -0800

    12.37 trang solution #21

---
+++ 12-coding-practice-problems/12.37-functions-rideshare-pickup-time/trang_solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int PickupMinutes(int userX, int userY, int d1X, int d1Y, int d2X, int d2Y, int d3X, int d3Y) {
+    int time1 = (d1X-userX) + (d1Y-userY);
+    int time2 = (d2X-userX) + (d2Y-userY);
+    int time3 = (d3X-userX) + (d3Y-userY);
+    int min = time1;
+
+    if (time2 < min) {
+        min = time2;
+    }
+    else if (time3 < min) {
+        min = time3;
+    }
+    return min*2;
+}
+
+int main() {
+   int userX, userY;
+   int d1X, d1Y;
+   int d2X, d2Y;
+   int d3X, d3Y;
+
+   cin >> userX;
+   cin >> userY;
+   cin >> d1X;
+   cin >> d1Y;
+   cin >> d2X;
+   cin >> d2Y;
+   cin >> d3X;
+   cin >> d3Y;
+
+   cout << PickupMinutes(userX, userY, d1X, d1Y, d2X, d2Y, d3X, d3Y) << endl;
+
+   return 0;
+}
